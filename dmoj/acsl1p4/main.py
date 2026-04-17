90fdcfdad3b88bbf884b43703f85d9c7e0f0a7de
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2026-04-16 23:30:38 -0400

    i lowk cheated on this one ngl chat :(

--- dmoj/acsl1p4/main.py
+++ dmoj/acsl1p4/main.py
@@ -5,16 +5,18 @@ splii = lambda: map(int, input().split())
 ii = lambda: int(input())
 # --- Boilerplate ---
 
-adj_list = defaultdict(list)
 n, k = splii()
+adj_list = { i:[] for i in range(n+1)}
+adj_list2 = { i:[] for i in range(n+1)}
+
 for _ in range(k):
   a, b, sa, sb = splii()
   if sa > sb:
     adj_list[a].append(b)
-    adj_list.setdefault(b, [])
+    adj_list2[b].append(a)
   else:
     adj_list[b].append(a)
-    adj_list.setdefault(a, [])
+    adj_list2[a].append(b)
 
 def remove(adj_list):
   for adj in adj_list:
@@ -23,5 +25,10 @@ def remove(adj_list):
       return remove(adj_list)
   return adj_list
 
-print(len(remove(adj_list)))
+a1 = remove(adj_list)
+a2 = remove(adj_list2)
+
+count = len(set(a1).intersection(set(a2)))
+
+print(count)
 
