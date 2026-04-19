f5cfe5dac219ae777e6aaf7dac13132286210f76
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2026-04-19 03:30:04 -0400

    c plus plus baby

--- cses/1068-weird-algorithm/main.py
+++ cses/1068-weird-algorithm/main.py
@@ -4,3 +4,13 @@ az = 'abcdefghijklmnopqrstuvwxyz'
 splii = lambda: map(int, input().split())
 ii = lambda: int(input())
 # --- Boilerplate ---
+
+n = ii()
+while n != 1:
+  print(n, end=' ')
+  if n % 2:
+    n = n * 3 + 1
+  else:
+    n = n // 2
+print(1)
+  
